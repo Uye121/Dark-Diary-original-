5edc66d593c30801d5df1b7b466e69b27795df37
Ulric Ye <yye90012@gmail.com>
2016-08-02 16:38:24 -0700

    Made extension GameScene file for levels. Added explosion sound.

---
+++ Dark Diary/GameScene(LevelExtension).swift
@@ -0,0 +1,129 @@
+//
+//  GameScene(extension).swift
+//  Dark Diary
+//
+//  Created by Ulric Ye on 8/2/16.
+//  Copyright © 2016 TestingDummies@_@. All rights reserved.
+//
+
+import SpriteKit
+
+extension GameScene {
+    
+    /////////////////////////////LEVELS/////////////////////////////
+    func level1() {
+        let resourcePath = NSBundle.mainBundle().pathForResource("Level1", ofType: "sks")
+        let level1 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
+        levelNode.addChild(level1)
+        lighting = self.childNodeWithName("//lighting") as! SKLightNode
+        levelBackground = childNodeWithName("//level1Background") as! SKSpriteNode
+        levels = 4
+        numberOfPages = 0
+        
+        state = .Playing
+        
+        levelWidth = levelBackground.size.width
+        levelHeight = levelBackground.size.height
+        
+        /* create 4 pages for level 1 */
+        while numberOfPages < levels {
+            createPage()
+            numberOfPages += 1
+            spawnOutside()
+        }
+    }
+    
+    func level2() {
+        let resourcePath = NSBundle.mainBundle().pathForResource("Level2", ofType: "sks")
+        let level2 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
+        levelNode.addChild(level2)
+        lighting = self.childNodeWithName("//lighting") as! SKLightNode
+        levelBackground = childNodeWithName("//level2Background") as! SKSpriteNode
+        levels = 6
+        numberOfPages = 0
+        numberOfBoxes = 0
+        
+        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
+        lightCamera.position = light1.position
+        
+        levelWidth = levelBackground.size.width
+        levelHeight = levelBackground.size.height
+        
+        /* Create pages and boxes */
+        while numberOfPages < levels {
+            createPage()
+            numberOfPages += 1
+            spawnOutside()
+        }
+        while numberOfBoxes < 3 {
+            createRandomBox()
+            numberOfBoxes += 1
+        }
+    }
+    
+    func level3() {
+        let resourcePath = NSBundle.mainBundle().pathForResource("Level3", ofType: "sks")
+        let level3 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
+        levelNode.addChild(level3)
+        lighting = self.childNodeWithName("//lighting") as! SKLightNode
+        levelBackground = childNodeWithName("//level3Background") as! SKSpriteNode
+        
+        levelWidth = levelBackground.size.width
+        levelHeight = levelBackground.size.height
+        
+        levels = 8
+        numberOfPages = 0
+        numberOfBoxes = 0
+        
+        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
+        lightCamera.position = light1.position
+        
+        /* Create pages and boxes */
+        while numberOfPages < levels {
+            createPage()
+            numberOfPages += 1
+            spawnOutside()
+        }
+        while numberOfBoxes < 3 {
+            createRandomBox()
+            numberOfBoxes += 1
+        }
+    }
+    
+    func level4() {
+        let resourcePath = NSBundle.mainBundle().pathForResource("Level4", ofType: "sks")
+        let level4 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
+        levelNode.addChild(level4)
+        lighting = self.childNodeWithName("//lighting") as! SKLightNode
+        levelBackground = childNodeWithName("//level4Background") as! SKSpriteNode
+        bombTimer = childNodeWithName("//bombTimer") as! SKLabelNode
+        
+        levelWidth = levelBackground.size.width
+        levelHeight = levelBackground.size.height
+        
+        createBomb()
+        bombTimer.moveToParent(bomb)
+        bombTimer.position.x = 1.971
+        bombTimer.position.y = -1.593
+        
+        levels = 8
+        numberOfPages = 0
+        numberOfBoxes = 0
+        
+        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
+        lightCamera.position = light1.position
+        
+        /* Create pages and boxes */
+        while numberOfPages < levels {
+            createPage()
+            numberOfPages += 1
+            spawnOutside()
+        }
+        while numberOfBoxes < 3 {
+            createRandomBox()
+            numberOfBoxes += 1
+        }
+        /* Calls function where the bomb counts down and leads to game over */
+        destruction()
+    }
+}
--- Dark Diary/GameScene.swift
+++ Dark Diary/GameScene.swift
@@ -441,6 +441,7 @@ class GameScene: SKScene {
     func destruction() {
         /* Code connect: bomb counting down */
         let bombBeep = SKAction.playSoundFileNamed("beep", waitForCompletion: false)
+        let explodingNoise = SKAction.playSoundFileNamed("ExplodingNoise", waitForCompletion: false)
         let wait = SKAction.waitForDuration(1)
         let block = SKAction.runBlock({
             if self.bombTime > 0 {
@@ -451,7 +452,9 @@ class GameScene: SKScene {
                 self.bombTimer.fontColor = UIColor.redColor()
             }
             if self.bombTime == 0 {
-                self.state = .GameOver
+                self.bomb.runAction(explodingNoise, completion: {
+                    self.state = .GameOver
+                })
             }
             self.bombTimer.text = "\(self.bombTime)"
         })
@@ -459,124 +462,6 @@ class GameScene: SKScene {
         bombTimeAction = SKAction.repeatActionForever(sequence)
         bombTimer.runAction(bombTimeAction)
     }
-    
-    
-    
-    /////////////////////////////LEVELS/////////////////////////////
-    func level1() {
-        let resourcePath = NSBundle.mainBundle().pathForResource("Level1", ofType: "sks")
-        let level1 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
-        levelNode.addChild(level1)
-        lighting = self.childNodeWithName("//lighting") as! SKLightNode
-        levelBackground = childNodeWithName("//level1Background") as! SKSpriteNode
-        levels = 4
-        numberOfPages = 0
-        
-        state = .Playing
-        
-        levelWidth = levelBackground.size.width
-        levelHeight = levelBackground.size.height
-        
-        /* create 4 pages for level 1 */
-        while numberOfPages < levels {
-            createPage()
-            numberOfPages += 1
-            spawnOutside()
-        }
-    }
-    
-    func level2() {
-        let resourcePath = NSBundle.mainBundle().pathForResource("Level2", ofType: "sks")
-        let level2 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
-        levelNode.addChild(level2)
-        lighting = self.childNodeWithName("//lighting") as! SKLightNode
-        levelBackground = childNodeWithName("//level2Background") as! SKSpriteNode
-        levels = 6
-        numberOfPages = 0
-        numberOfBoxes = 0
-        
-        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
-        lightCamera.position = light1.position
-        
-        levelWidth = levelBackground.size.width
-        levelHeight = levelBackground.size.height
-        
-        /* Create pages and boxes */
-        while numberOfPages < levels {
-            createPage()
-            numberOfPages += 1
-            spawnOutside()
-        }
-        while numberOfBoxes < 3 {
-            createRandomBox()
-            numberOfBoxes += 1
-        }
-    }
-    
-    func level3() {
-        let resourcePath = NSBundle.mainBundle().pathForResource("Level3", ofType: "sks")
-        let level3 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
-        levelNode.addChild(level3)
-        lighting = self.childNodeWithName("//lighting") as! SKLightNode
-        levelBackground = childNodeWithName("//level3Background") as! SKSpriteNode
-        
-        levelWidth = levelBackground.size.width
-        levelHeight = levelBackground.size.height
-        
-        levels = 8
-        numberOfPages = 0
-        numberOfBoxes = 0
-        
-        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
-        lightCamera.position = light1.position
-        
-        /* Create pages and boxes */
-        while numberOfPages < levels {
-            createPage()
-            numberOfPages += 1
-            spawnOutside()
-        }
-        while numberOfBoxes < 3 {
-            createRandomBox()
-            numberOfBoxes += 1
-        }
-    }
-    
-    func level4() {
-        let resourcePath = NSBundle.mainBundle().pathForResource("Level4", ofType: "sks")
-        let level4 = SKReferenceNode (URL: NSURL (fileURLWithPath: resourcePath!))
-        levelNode.addChild(level4)
-        lighting = self.childNodeWithName("//lighting") as! SKLightNode
-        levelBackground = childNodeWithName("//level4Background") as! SKSpriteNode
-        bombTimer = childNodeWithName("//bombTimer") as! SKLabelNode
-        
-        levelWidth = levelBackground.size.width
-        levelHeight = levelBackground.size.height
-        
-        createBomb()
-        bombTimer.moveToParent(bomb)
-        bombTimer.position.x = 1.971
-        bombTimer.position.y = -1.593
-        
-        levels = 8
-        numberOfPages = 0
-        numberOfBoxes = 0
-        
-        light1.position = CGPoint(x: screenWidth/2, y: screenHeight/2)
-        lightCamera.position = light1.position
-        
-        /* Create pages and boxes */
-        while numberOfPages < levels {
-            createPage()
-            numberOfPages += 1
-            spawnOutside()
-        }
-        while numberOfBoxes < 3 {
-            createRandomBox()
-            numberOfBoxes += 1
-        }
-        /* Calls function where the bomb counts down and leads to game over */
-        destruction()
-    }
 }
+
 
